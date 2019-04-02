bdce9928b81b47458a325a81b941b88157f0b4c7
Aarone Koshy <aarone.k.496@gmail.com>
2019-04-02 17:54:25 +1300

    Smooth collision detection no comments

--- src/BoxController.java
+++ src/BoxController.java
@@ -6,20 +6,29 @@ public class BoxController {
 
     CharacterView view;
 
-    public BoxController(){
-        box = new Rectangle(1650, 1500, 50,50);
+    public BoxController() {
+        box = new Rectangle(1650, 1500, 50, 50);
         view = new CharacterView(box);
     }
 
-    public void update(){
+    public void update() {
         view.moveWorld(0, 0);
     }
 
-    public Vector2 move(Vector2 v, Rectangle player){
-        Rectangle projRect = new Rectangle(player);
-        projRect.setLocation((int) (player.x + v.x), (int) (player.y + v.y));
-        if (projRect.intersects(box)){
-            return new Vector2(0,0);
+    public Vector2 move(Vector2 v, Rectangle player) {
+
+        Rectangle projRectup = new Rectangle(player);
+        projRectup.setSize(player.width, (int) (player.height * 0.95));
+        projRectup.setLocation((int) (player.x + v.x), (int) (player.y));
+        if (projRectup.intersects(box)) {
+            v.x = 0;
+        }
+
+        Rectangle projRecty = new Rectangle(player);
+        projRecty.setSize((int) (player.width * 0.95),player.height);
+        projRecty.setLocation((int) (player.x), (int) (player.y + v.y));
+        if (projRecty.intersects(box)) {
+            v.y = 0;
         }
         return v;
     }
