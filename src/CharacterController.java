a4c5d750b56d7ccce096910ef89ea160bd910460
Minpin04 <aarone.k.496@gmail.com>
2019-04-25 10:17:30 +1200

    Added Colliding Enemy

---
+++ src/CharacterController.java
@@ -0,0 +1,73 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class CharacterController {
+    private float x = 0;
+    private float y = 0;
+
+    public double deltaTime = 0;
+    public float speed = 0.2f;
+
+    CharacterView view;
+    Character model;
+    public BoxManager boxManager;
+    private BoxController boxController;
+
+    public CharacterController(Point spawnPos, BoxManager _boxManager) {
+        try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+                @Override
+                public void run() {
+                    model = new Character(new Rectangle(spawnPos.x, spawnPos.y, 50, 50), 2);
+                    view = new CharacterView(new Rectangle(spawnPos.x, spawnPos.y, 50, 50), model);
+                    boxController = new BoxController(model, view);
+                    boxManager = _boxManager;
+                    boxManager.colliders.add(boxController);
+                }
+            });
+        } catch (Exception e) {
+            // Required to catch potential exception
+        }
+    }
+
+    //Moves player based on key inputs
+    //delta is equal to newPosition - oldPosition so if 0 the player will stand still
+    public void update() {
+        groundMovement();
+
+        try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+                @Override
+                public void run() {
+                    view.moveWorld((int) x, (int) y);
+                }
+            });
+        } catch (Exception e) {
+            // Required to catch potential exception
+        }
+    }
+
+    public void groundMovement(){
+        double deltaX = 0, deltaY = 0;
+        if (KeyStates.moveRightKey.keyState())
+            deltaX = deltaTime * speed;
+        if (KeyStates.moveLeftKey.keyState())
+            deltaX = -deltaTime * speed;
+        if (KeyStates.moveForwardKey.keyState())
+            deltaY = -deltaTime * speed;
+        if (KeyStates.moveBackwardsKey.keyState())
+            deltaY = deltaTime * speed;
+        //Checks with the box manager if it will hit a box and returns movement vector based on collisions
+        Vector2 v = boxManager.move(new Vector2((float) deltaX, (float) deltaY), view.getBounds(), boxController);
+        x += v.x;
+        y += v.y;
+    }
+
+    public JPanel getView() {
+        return view;
+    }
+
+    public Point getPos() {
+        return view.getPos();
+    };
+}
