e19176eac357e3f4b352ea1750cf5fe166df328b
itsmejoeeey <9375730+itsmejoeeey@users.noreply.github.com>
2019-04-29 05:26:30 +1200

    Added tilemap to misc

---
+++ misc/tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="tilemap" tilewidth="32" tileheight="32" tilecount="1740" columns="58">
+ <image source="../src/res/textures/tilemap.png" width="1856" height="960"/>
+</tileset>
