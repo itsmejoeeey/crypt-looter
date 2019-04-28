a48fd2298408a04cb00f561b1845363321d072a6
itsmejoeeey <9375730+itsmejoeeey@users.noreply.github.com>
2019-04-28 23:10:02 +1200

    Removed files that should have been removed to begin with

--- out/production/2019-Java-Group5/maps/tilemap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="2019.04.25" name="tilemap" tilewidth="32" tileheight="32" tilecount="1710" columns="57">
- <image source="../../../../../Pictures/tilemap.png" width="1853" height="960"/>
-</tileset>
--- out/production/2019-Java-Group5/res/maps/tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="2019.04.18" name="tileset" tilewidth="32" tileheight="32" tilecount="667" columns="23">
- <image source="C:/Users/jmil565/Projects/2019-Java-Group5/src/res/textures/tileset.png" width="736" height="928"/>
-</tileset>
